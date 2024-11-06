4ce36a2e7b909c2efaf84bb4455f17114e21af20
Stas <yatouag@gmail.com>
2024-11-06 10:32:27 +0300

    Новое изображение открывается в новом окне

--- Cat1.py
+++ Cat1.py
@@ -18,10 +18,14 @@ def load_image(url):
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title("Картинка с котиком")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.pack()
         label.image = img
 
 
@@ -34,8 +38,7 @@ window = Tk()
 window.title("Cats")
 window.geometry("600x520")
 
-label = Label()
-label.pack()
+
 
 # update_button = Button(text="Обновить", command=set_image)
 # update_button.pack()
@@ -45,13 +48,11 @@ window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
-
-set_image()
 
 window.mainloop()
 
