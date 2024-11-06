ebc84d9a1a08b5de204c571561f1e68d82454146
Stas <yatouag@gmail.com>
2024-11-06 11:01:27 +0300

    Добавленно получение изображений по тегу, а также без тега.

--- Cat1.py
+++ Cat1.py
@@ -19,7 +19,10 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag
+                     )
     if img:
         new_window = Toplevel()
         new_window.title("Картинка с котиком")
@@ -38,7 +41,11 @@ window = Tk()
 window.title("Cats")
 window.geometry("600x520")
 
+tag_entry = Entry()
+tag_entry.pack()
 
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 # update_button = Button(text="Обновить", command=set_image)
 # update_button.pack()
