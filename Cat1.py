2ff71a84d62dec63b33b2a117bcdf8b90de94be5
Stas <yatouag@gmail.com>
2024-11-06 10:14:43 +0300

    Добавили изменение размера изображения

--- Cat1.py
+++ Cat1.py
@@ -11,6 +11,7 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600,480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
@@ -26,12 +27,13 @@ def set_image():
 
 window = Tk()
 window.title("Cats")
-window.geometry("600x480")
+window.geometry("600x520")
 
 label = Label()
 label.pack()
 
 update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
 
 url = "https://cataas.com/cat"
 
